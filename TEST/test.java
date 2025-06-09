98b3405ff2080079e4dd9636823c27af28579538
Vijay Kumar <vijay.kumar_cs23@gla.ac.in>
2025-06-09 10:41:45 +0530

    new line added

--- TEST/test.java
+++ TEST/test.java
@@ -1,5 +1,7 @@
 public class test{
     public static void main (String []args){
-     System.out.print("Hello World");
+     System.out.println("Hello World");
+     System.out.println("Hi");
+
     }
 }
