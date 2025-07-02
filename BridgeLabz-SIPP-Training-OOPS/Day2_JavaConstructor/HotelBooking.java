676b7aac77bd2b296960d7dc8b244c75397a0398
Vijay Kumar <vijay.kumar_cs23@gla.ac.in>
2025-07-02 12:00:55 +0530

    [Vijay Kumar] HotelBooking

---
+++ BridgeLabz-SIPP-Training-OOPS/Day2_JavaConstructor/HotelBooking.java
@@ -0,0 +1,49 @@
+package Day2_JavaConstructor;
+
+public class HotelBooking {
+    private String guestName;
+    private String roomType;
+    private int nights;
+
+    // Default constructor
+    public HotelBooking() {
+        this.guestName = "Guest";
+        this.roomType = "Standard";
+        this.nights = 1;
+    }
+
+    // Parameterized constructor
+    public HotelBooking(String guestName, String roomType, int nights) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+        this.nights = nights;
+    }
+
+    // Copy constructor
+    public HotelBooking(HotelBooking booking) {
+        this.guestName = booking.guestName;
+        this.roomType = booking.roomType;
+        this.nights = booking.nights;
+    }
+
+    public void displayDetails() {
+        System.out.println("Guest Name: " + guestName);
+        System.out.println("Room Type: " + roomType);
+        System.out.println("Number of Nights: " + nights);
+    }
+
+    public static void main(String[] args) {
+        HotelBooking defaultBooking = new HotelBooking();
+        HotelBooking customBooking = new HotelBooking("Nishant", "Deluxe", 3);
+        HotelBooking copiedBooking = new HotelBooking(customBooking);
+
+        System.out.println("Default Booking:");
+        defaultBooking.displayDetails();
+
+        System.out.println("\nCustom Booking:");
+        customBooking.displayDetails();
+
+        System.out.println("\nCopied Booking:");
+        copiedBooking.displayDetails();
+    }
+}
