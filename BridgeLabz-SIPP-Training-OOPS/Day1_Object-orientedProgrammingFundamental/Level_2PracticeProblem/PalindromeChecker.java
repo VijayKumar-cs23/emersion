0fb94717cd52ccbaa732a240d4d7aa5ba0e84012
Vijay Kumar <vijay.kumar_cs23@gla.ac.in>
2025-07-02 11:52:32 +0530

    [Vijay Kumar] PalindromeChecker

---
+++ BridgeLabz-SIPP-Training-OOPS/Day1_Object-orientedProgrammingFundamental/Level_2PracticeProblem/PalindromeChecker.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class PalindromeChecker {
+    String text;
+
+    PalindromeChecker(String text) {
+        this.text = text;
+    }
+
+    boolean isPalindrome() {
+        String cleaned = text.replaceAll("\\s+", "").toLowerCase();
+        int left = 0;
+        int right = cleaned.length() - 1;
+
+        while (left < right) {
+            if (cleaned.charAt(left) != cleaned.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    void displayResult() {
+        if (isPalindrome()) {
+            System.out.println("\"" + text + "\" is a palindrome.");
+        } else {
+            System.out.println("\"" + text + "\" is not a palindrome.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter text to check: ");
+        String input = sc.nextLine();
+
+        PalindromeChecker checker = new PalindromeChecker(input);
+        checker.displayResult();
+
+        sc.close();
+    }
+}
